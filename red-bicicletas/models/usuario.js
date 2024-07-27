787cffca618d343def9042b7deeda8bf52cd8040
Rodrigo Llull Torres <rllullt@gmail.com>
2024-07-26 21:06:45 -0400

    Basic user authentication name, email, password, verified email, encrypted password

--- red-bicicletas/models/usuario.js
+++ red-bicicletas/models/usuario.js
@@ -1,10 +1,54 @@
 const mongoose = require('mongoose');
 const Reserva = require('./reserva');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;  // introduces some randomness to a ‘cryptonization’
+
+const validateEmail = function(email) {
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+}
 
 const usuarioSchema = new Schema({
-    nombre: String,
+    nombre: {
+        type: String,
+        trim: true,
+        required: [true, 'El nombre es obligatorio'],
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'El email es obligatorio'],
+        lowercase: true,
+        validate: [validateEmail, 'Por favor, ingrese un email válido'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/],
+    },
+    password: {
+        type: String,
+        required: [true, 'El password es obligatorio'],
+    },
+    passwordResetToken: String,
+    passwordResetTokenExpires: Date,
+    verificado: {
+        type: Boolean,
+        default: false,
+    },
 });
+
+/**
+ * Siempre, antes de que se ejecute un save, se ejecutará la función
+ */
+usuarioSchema.pre('save', function(next) {
+    if (this.isModified('password')) {
+        this.password = bcrypt.hashSync(this.password, saltRounds);
+    }
+    next();
+});
+
+usuarioSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+}
 
 usuarioSchema.statics.allUsuarios = function() {
     // this hace referencia al schema. find recibe el filtro (vacío en este caso) + el callback
