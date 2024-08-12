28536a3c9b83e4aa2384f7e9505a2c52d0c3ff81
Rodrigo Llull Torres <rllullt@gmail.com>
2024-08-12 17:49:30 -0400

    Added Passport module and partial support

--- red-bicicletas/app.js
+++ red-bicicletas/app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const passport = require('./config/passport');
+const session = require('express-session');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -12,7 +14,16 @@ const bicicletasRouter = require('./routes/bicicletas');
 const bicicletasAPIRouter = require('./routes/api/bicicletas');
 const usuariosAPIRouter = require('./routes/api/usuarios');
 
+const store = new session.MemoryStore;
+
 const app = express();
+app.use(session({
+  cookie: { maxAge: 240 * 60 * 60 * 1000 },  // 240 hrs
+  store: store,
+  saveUninitialized: true,
+  resave: 'true',
+  secret: 'red_bicis_!!!***!",!",!",!",123123',  // cualquier string
+}));
 
 /**
  * Get database from environment and store in Express
@@ -31,7 +42,31 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Login steps
+app.get('/login', function(req, res) {
+  res.render('session/login');
+});
+
+app.post('/login', function(req, res, next) {
+  // passport
+});
+
+app.get('/logout', function(req, res) {
+  res.redirect('/');
+});
+
+app.get('/forgotPassword', function(req, res) {
+  // nada
+});
+
+app.post('/forgotPassword', function(req, res) {
+  // nada
+});
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
